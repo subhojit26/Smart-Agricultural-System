681a35b224ca4c8b91c7ec46b0c6dbf676e38451
Shubham-Rasal <bluequbits@gmail.com>
2024-11-07 07:35:14 +0530

    update search edge function

--- schemes/supabase/functions/search/index.ts
+++ schemes/supabase/functions/search/index.ts
@@ -1,25 +1,33 @@
-const model = new Supabase.ai.Session('gte-small')
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+
+import { createClient } from "jsr:@supabase/supabase-js@2";
+const model = new Supabase.ai.Session("gte-small");
 
 Deno.serve(async (req) => {
-  const { search } = await req.json()
-  if (!search) return new Response('Please provide a search param!')
+  const { search } = await req.json();
+  if (!search) return new Response("Please provide a search param!");
   // Generate embedding for search term.
-  const embedding = await model.run(search, {
+  const input_embedding = await model.run(search, {
     mean_pool: true,
     normalize: true,
-  })
+  });
+
+  const supabase = createClient<Database>(
+    Deno.env.get("SUPABASE_URL")!,
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
+  );
 
   // Query embeddings.
   const { data: result, error } = await supabase
-    .rpc('query_embeddings', {
-      embedding,
+    .rpc("query_embeddings", {
+      input_embedding,
       match_threshold: 0.8,
     })
-    .select('content')
-    .limit(3)
+    .select("content")
+    .limit(3);
   if (error) {
-    return Response.json(error)
+    return Response.json(error);
   }
 
-  return Response.json({ search, result })
-})
+  return Response.json({ search, result });
+});
